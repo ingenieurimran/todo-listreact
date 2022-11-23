e5509b66c48b6f1ad9cf709aad463f13079522e0
iali <engr.imran_1242@hotmail.com>
2022-11-23 15:34:22 +0100

    final app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,75 +1,76 @@
-import React, { useState } from 'react'
-import ToDoItem from './ToDoItem'
-import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
-import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
 
 function App() {
-  const [textInput, setTextInput] = useState('')
-  const [items, setItems] = useState([])
-  const [isToggleSubmit, setIsToggleSubmit] = useState(true)
-  const [isNeuItemId, setIsNeuItemId] = useState(null)
+  const [textInput, setTextInput] = useState("");
+  const [items, setItems] = useState([]);
+  const [isToggleSubmit, setIsToggleSubmit] = useState(true);
+  const [isNeuItemId, setIsNeuItemId] = useState(null);
   function handleChange(params) {
-    const newTextInput = params.target.value
-    setTextInput(newTextInput)
+    const newTextInput = params.target.value;
+    setTextInput(newTextInput);
   }
   function handleClick(params) {
     const textInputNew = {
       id: new Date().getTime().toString(),
       name: textInput,
-    }
+    };
     if (!textInput) {
     } else if (textInput && !isToggleSubmit) {
       setItems((preValue) => {
         return preValue.map((item) => {
           if (item.id === isNeuItemId) {
-            return { ...item, name: textInput }
+            return { ...item, name: textInput };
           } else {
-            return item
+            return item;
           }
-        })
-      })
-      setIsToggleSubmit(true)
-      setTextInput('')
-      setIsNeuItemId(null)
+        });
+      });
+      setIsToggleSubmit(true);
+      setTextInput("");
+      setIsNeuItemId(null);
     } else {
       setItems((preValue) => {
-        return [...preValue, textInputNew]
-      })
+        return [...preValue, textInputNew];
+      });
     }
 
-    setTextInput('')
+    setTextInput("");
   }
 
   function handleDelete(id) {
     setItems((preItems) => {
       return preItems.filter((item) => {
-        return item.id !== id
-      })
-    })
+        return item.id !== id;
+      });
+    });
   }
 
   function handleEdit(id) {
     let newTextInput = items.find((item) => {
-      return item.id === id
-    })
-    setIsToggleSubmit(false)
-    setTextInput(newTextInput.name)
-    setIsNeuItemId(id)
+      return item.id === id;
+    });
+    setIsToggleSubmit(false);
+    setTextInput(newTextInput.name);
+    setIsNeuItemId(id);
   }
 
   return (
     <div className="container">
       <div className="heading">
-        <h1>React JS</h1>
+        <h1>ToDo App</h1>
       </div>
       <div className="form">
         <input onChange={handleChange} value={textInput} type="text" />
         {isToggleSubmit ? (
-          <AddCircleOutlineOutlinedIcon onClick={handleClick} style={{fontSize:'2.5rem'}}/>
+          <AddCircleOutlineOutlinedIcon
+            onClick={handleClick}
+            style={{ fontSize: "2.5rem" }}
+          />
         ) : (
-          <EditOutlinedIcon
-            title="Add Item"
-            onClick={handleClick} />
+          <EditOutlinedIcon title="Add Item" onClick={handleClick} />
         )}
       </div>
       <div>
@@ -86,7 +87,7 @@ function App() {
         </ul>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
